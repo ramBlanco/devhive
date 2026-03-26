6b765ef2d7e3e55962858462e12e269f60ea8d59
Ramses <ramses2.blanco@gmail.com>
2026-03-26 19:26:52 -0300

    feat: v0.1.6

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="devhive-mcp",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "devhive-mcp=mcp_server.server:main",
+            "devhive_mcp=mcp_server.server:main",
             "devhive=mcp_server.cli:main",
         ],
     },
