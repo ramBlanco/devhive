2e5c788c787aa94379c021356cdecdacb91d810e
Ramses <ramses2.blanco@gmail.com>
2026-03-26 20:57:26 -0300

    fix: devhive name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="devhive-mcp",
-    version="0.1.11",
+    name="devhive",
+    version="0.1.12",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
